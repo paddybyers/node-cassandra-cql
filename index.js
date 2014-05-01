af603f6e5dab1cb5b0991675a80efec84e66c935
Dan McAulay <dmcaulay@gmail.com>
2014-05-01 15:36:07 -0700

    the client is now using the new connection class

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ var optionsDefault = {
   staleTime: 1000,
   //maximum amount of times an execute can be retried (using another connection) because of an unhealthy server response
   maxExecuteRetries: 3,
-  //maximum time (in milliseconds) to get a healthy connection from the pool. It should be connection Timeout * n.
-  getAConnectionTimeout: 3500,
+  //maximum time (in milliseconds) to get a healthy connection from the pool. It should be connection timeout * 2.
+  getAConnectionTimeout: 2000,
   //number of connections to open for each host
   poolSize: 1
 };
@@ -48,6 +48,7 @@ Client.prototype._createPool = function () {
       var connOptions = utils.extend({}, this.options, {host: host[0], port: host[1] || 9042});
       var c = new Connection(connOptions);
       c.indexInPool = (this.options.poolSize * poolIndex) + i;
+      c.on('disconnect', this._removeAllPrepared.bind(this));
       this.connections.push(c);
     }
   }
@@ -55,123 +56,68 @@ Client.prototype._createPool = function () {
   this.emit('log', 'info', this.connections.length + ' connections created across ' + this.options.hosts.length + ' hosts.');
 };
 
-/**
- * Connects to each host
+/** 
+ * Connects to all hosts, in case the pool is disconnected.
+ * @param {function} callback is called when the pool is connected (or at least 1 connected and the rest failed to connect) or it is not possible to connect 
  */
-Client.prototype._connectAllHosts = function (connectCallback) {
+Client.prototype.connect = function(callback) {
+  if (this.connected) return callback();
+  
   this.emit('log', 'info', 'Connecting to all hosts');
   var errors = [];
-  this.connecting = true;
   var self = this;
   async.each(this.connections, 
     function (c, callback) {
       c.open(function (err) {
         if (err) {
-          self._setUnhealthy(c);
-          errors.push(err);
+          errors.push({connection: c, error: err});
           self.emit('log', 'error', 'There was an error opening connection #' + c.indexInPool, err);
-        }
-        else {
+        } else {
           self.emit('log', 'info', 'Opened connection #' + c.indexInPool);
         }
         callback();
       });
     },
     function () {
-      self.connecting = false;
-      var error = null;
       if (errors.length === self.connections.length) {
-        error = new PoolConnectionError(errors);
+        errors = errors.map(function(e) { return e.error; })
+        return callback(new PoolConnectionError(error));
       }
-      self.connected = !error;
-      connectCallback(error);
-      self.emit('connection', error);
-    });
-};
-
-/** 
- * Connects to all hosts, in case the pool is disconnected.
- * @param {function} callback is called when the pool is connected (or at least 1 connected and the rest failed to connect) or it is not possible to connect 
- */
-Client.prototype.connect = function (callback) {
-  if (!callback) {
-    callback = function () {};
-  }
-  if (this.connected || this.connectionError) {
-    callback(this.connectionError);
-    return;
-  }
-  if (this.connecting) {
-    //queue while is connecting
-    this.emit('log', 'info', 'Waiting for the pool to connect');
-    this.once('connection', callback);
-    return;
-  }
-  //it is the first time. Try to connect to all hosts
-  var self = this;
-  this.connecting = true;
-  this._connectAllHosts(function (err) {
-    if (err) {
-      self.connectionError = err;
+      this.connected = true;
+      // start connection loop for failed connections
+      errors.forEach(function(e) { errors.c.open(); });
+      callback();
     }
-    callback(err);
-  });
+  );
 };
 
 /**
  * Gets a live connection
  * If there isn't an active connection available, it calls the callback with the error.
  */
-Client.prototype._getAConnection = function (callback) {
-  var self = this;
-  self.connect(function (err) {
-    if (err) {
-      callback(err);
-      return;
+Client.prototype._getAConnection = function(callback) {
+  var startTime = Date.now();
+
+  getConnectionLoop();
+
+  function getConnectionLoop() {
+    if (Date.now() - startTime > self.options.getAConnectionTimeout) {
+      return callback(new types.TimeoutError('Get a connection timed out'));
     }
-    //go through the connections
-    //watch out for infinite loops
-    var startTime = Date.now();
-    function checkNextConnection (callback) {
-      self.emit('log', 'info', 'Checking next connection');
-      self.connectionIndex = self.connectionIndex + 1;
-      if (self.connectionIndex > self.connections.length-1) {
-        self.connectionIndex = 0;
-      }
+
+    function nextIndex() { return (self.connectionIndex + 1) % self.connections.length; }
+
+    var startIndex = self.connectionIndex = nextIndex();
+    do {
       var c = self.connections[self.connectionIndex];
-      if (self._isHealthy(c)) {
-        callback(null, c);
-      }
-      else if (Date.now() - startTime > self.options.getAConnectionTimeout) {
-        callback(new types.TimeoutError('Get a connection timed out'));
-      }
-      else if (!c.connecting && self._canReconnect(c)) {
-        self.emit('log', 'info', 'Retrying to open #' + c.indexInPool);
-        //try to reconnect
-        c.open(function(err){
-          if (err) {
-            //This connection is still not good, go for the next one
-            self._setUnhealthy(c);
-            setTimeout(function () {
-              checkNextConnection(callback);
-            }, 10);
-          }
-          else {
-            //this connection is now good
-            self._setHealthy(c);
-            callback(null, c);
-          }
-        });
-      }
-      else {
-        //this connection is not good, try the next one
-        setTimeout(function () {
-          checkNextConnection(callback);
-        }, 10);
-      }
-    }
-    checkNextConnection(callback);
-  });
+      if (c.writable()) return callback(null, c);
+
+      self.connectionIndex = nextIndex();
+    } while(startIndex != self.connectionIndex);
+
+    // no available connects loop again
+    setTimeout(getConnectionLoop, 500);
+  }
 };
 
 /**
@@ -201,9 +147,6 @@ Client.prototype.execute = function () {
         }
         self.emit('log', 'info', util.format('connection #%d acquired, executing query: %s', c.indexInPool, args.query));
         c.execute(args.query, args.params, args.consistency, function(err, result) {
-          if (self._isServerUnhealthy(err)) {
-            self._setUnhealthy(c);
-          }
           executeError = err;
           executionResult = result;
           next();
@@ -244,7 +187,6 @@ Client.prototype.executeAsPrepared = function () {
   self._getPrepared(args.query, function preparedCallback(err, con, queryId) {
     if (self._isServerUnhealthy(err)) {
       //its a fatal error, the server died
-      self._setUnhealthy(con);
       args.options = utils.extend({retryCount: 0}, args.options);
       if (args.options.retryCount === self.options.maxExecuteRetries) {
         return args.callback(err);
@@ -389,9 +331,6 @@ Client.prototype.executeBatch = function (queries, consistency, options, callbac
         }
         self.emit('log', 'info', util.format('connection #%d acquired, executing batch', c.indexInPool));
         c.executeBatch(args.queries, args.consistency, args.options, function (err) {
-          if (self._isServerUnhealthy(err)) {
-            self._setUnhealthy(c);
-          }
           executeError = err;
           next();
         });
@@ -444,9 +383,6 @@ Client.prototype._executeOnConnection = function (c, query, queryId, params, con
   var self = this;
   c.executePrepared(queryId, params, consistency, options, function(err, result1, result2) {
     if (self._isServerUnhealthy(err)) {
-      //There is a problem with the connection/server that had a prepared query
-      //forget about this connection for now
-      self._setUnhealthy(c);
       //retry the whole thing, it will get another connection
       self.executeAsPrepared(query, params, consistency, options, callback);
     }
@@ -543,42 +479,13 @@ Client.prototype._isServerUnhealthy = function (err) {
   return err && err.isServerUnhealthy;
 };
 
-Client.prototype._setUnhealthy = function (connection) {
-  if (!connection.unhealthyAt) {
-    this.emit('log', 'error', 'Connection #' + connection.indexInPool + ' is being set to Unhealthy');
-    connection.unhealthyAt = new Date().getTime();
-    this._removeAllPrepared(connection);
-  }
-};
-
-Client.prototype._setHealthy = function (connection) {
-  connection.unhealthyAt = 0;
-  this.emit('log', 'info', 'Connection #' + connection.indexInPool + ' was set to healthy');
-};
-
-Client.prototype._canReconnect = function (connection) {
-  var timePassed = new Date().getTime() - connection.unhealthyAt;
-  return timePassed > this.options.staleTime;
-};
-
-/**
- * Determines if a connection can be used
-*/
-Client.prototype._isHealthy = function (connection) {
-  return !connection.unhealthyAt;
-};
-
 /**
  * Closes all connections
  */
 Client.prototype.shutdown = function (callback) {
   async.each(this.connections, function(c, eachCallback) {
-    c.close(eachCallback);
-  }, function() {
-    if (callback) {
-      callback();
-    }
-  });
+    c.shutdown(eachCallback);
+  }, callback || function() { });
 };
 
 /**
